7eec28662b5e09978d9d9541f532bd50b6126e58
Muhammed Mortgy <m@mortgy.com>
2022-07-07 15:22:21 +0300

    add close position endpoint

---
+++ src/main/java/com/ismail/dukascopy/model/ClosePositionResp.java
@@ -0,0 +1,32 @@
+package com.ismail.dukascopy.model;
+
+import com.dukascopy.api.IOrder.State;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * Close Position Response
+ * 
+ * @author Muhammed Mortgy
+ * @since 20220707
+ */
+@Data
+@ToString
+@JsonInclude(Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ClosePositionResp
+{           
+
+    public String clientOrderID = null;
+
+    public State orderState = null;
+    
+    public boolean closeSuccess = false;
+    
+    public String rejectReason = null;
+  
+}
